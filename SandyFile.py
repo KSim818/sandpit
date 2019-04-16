6e6e23b2231c079ce04e9e2b95bbb02a63cab653
Katie Simpson <Katie.Simpson@ecan.govt.nz>
2019-04-16 14:19:28 +1200

    created sandy file
    does not much

---
+++ SandyFile.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 16 14:14:27 2019
+
+@author: KatieSi
+"""
+
+# hey its a sandpit file
+
+# we can do anything here
+
+# don't link it to prod ;)
+
+x= 2
+
+print (x)
+
+x= x+2
+
+print (x)
