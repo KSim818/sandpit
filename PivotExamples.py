ad2413bdaf8aab57ef2e71e938ae34faf257f412
Katie Simpson <Katie.Simpson@ecan.govt.nz>
2019-04-18 16:56:07 +1200

    Create PivotExamples.py

---
+++ PivotExamples.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 18 16:14:01 2019
+
+@author: KatieSi
+"""
+
+# https://jakevdp.github.io/PythonDataScienceHandbook/03.09-pivot-tables.html
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+titanic = sns.load_dataset('titanic')
